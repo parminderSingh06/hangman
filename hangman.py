a371838c33f1f2f1b208e7f7754d7ec47825f814
Parminder Singh <parmindersingh1086@gmail.com>
2022-11-20 15:53:56 -0800

    Basic Setup
    added difficulty choice and a way to read words in from a file
    and store them in lists based on difficulty.

--- hangman.py
+++ hangman.py
@@ -1,3 +1,22 @@
+import random
+
 if __name__ == '__main__':
      print("Hello, Welcome to the Hangman Game.")
-     
+     file = open('words.txt','r')
+     tempWords = file.read().splitlines()
+     easyWords = []
+     mediumWords = []
+     hardWords = []
+     lives = 6
+     body = {6:'Left Arm',5:'Right Arm',4:'Left Leg',3:'Right Leg',2:'Torso',1:'Head'}
+     diff = {"easy": easyWords, "medium": mediumWords, "hard": hardWords}
+     for i in tempWords:
+          if len(i) < 4:
+             easyWords.append(i)
+          elif len(i) >= 4 and len(i) < 7:
+               mediumWords.append(i)
+          else:
+               hardWords.append(i)
+     c = input("Choose Your Difficulty(Easy   Medium   Hard):").lower()
+     word = random.choice(diff[c])
+          
